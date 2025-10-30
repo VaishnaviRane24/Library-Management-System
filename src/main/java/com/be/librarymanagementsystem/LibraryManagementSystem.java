b324ce223ee1b6a4362967c12cf9d99b2972cf6e
Vaishnavi Rane <ranevaishnavi242@gmail.com>
2025-10-30 16:55:30 +0530

    Initial commit

---
+++ src/main/java/com/be/librarymanagementsystem/LibraryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.be.librarymanagementsystem;
+
+/**
+ *
+ * @author Admin
+ */
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
